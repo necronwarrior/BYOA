0b81eb1f254c68fdad07b3070933873a8b2bc16e
unknown <1302534@MC10662.uad.ac.uk>
2016-12-10 17:51:43 +0000

    Finilised rotation and file info loading

---
+++ Assets/Plugins/FileRead.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+
+public class FileRead : MonoBehaviour {
+
+	public string[,] cardlist;
+	string filepath = "Assets/Resources/Cards.txt";
+
+	public static FileRead use = null;
+
+	void Awake()
+	{
+		if (use)
+		{
+			Debug.LogWarning("Only one instance of the FileRead script in a scene is allowed");
+			return;
+		}
+		use = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		string[] filelines = File.ReadAllLines (filepath);
+		cardlist = new string[filelines.Length,5];
+
+		for (int i =0;i< filelines.Length;i++)
+		{
+			string[] parts = filelines [i].Split('/');
+			cardlist[i, 0] = parts [0];
+			cardlist[i, 1] = parts [1];
+			cardlist[i, 2] = parts [2];
+			cardlist[i, 3] = parts [3];
+			cardlist[i, 4] = parts [4];
+		}
+	}
+	
+	public string[] CardFind(string CardName)
+	{
+		string[] currentcard = new string[5];
+		for (int i=0;i<cardlist.GetLength(0);i++)
+		{
+			if(cardlist[i,0] == CardName)
+			{
+				currentcard [0] =cardlist [i, 0];
+				currentcard [1] = cardlist [i, 1]; 
+				currentcard [2] =cardlist[i, 2]; 
+				currentcard [3] =cardlist[i, 3]; 
+				currentcard [4] =cardlist[i, 4]; 
+				break;
+			}
+		}
+		return currentcard;
+	}
+}
---
+++ Assets/Resources/TEMP/CardProperties.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardProperties : MonoBehaviour {
+
+	float CurrentRotation;
+	string[] CardStats;
+	public float incrementDelta, newcurr, x, y ,z;
+
+	// Use this for initialization
+	void Start () {
+		SetCard ("Test");
+	}
+
+	void SetCard(string CardName)
+	{
+		CardStats = new string[5];
+				CardStats = FileRead.use.CardFind (CardName);
+		z = transform.rotation.eulerAngles.z;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		x = transform.rotation.eulerAngles.x;
+		y = transform.rotation.eulerAngles.y;
+		z = transform.rotation.eulerAngles.z;
+
+		incrementDelta += (Time.deltaTime / 10.0f);
+	}
+
+	void OnMouseDown(){
+		StartCoroutine(MoveObject.use.Rotation (transform, Vector3.up*180.0f, 1.0f));
+	}
+}
